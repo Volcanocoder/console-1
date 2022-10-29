4377d0401c6bf03c22e270622ce4c4e3d423758c
Ali Na <aliwpna@gmail.com>
2022-10-29 18:37:30 +0330

    first init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Booleans
+    {
+        static void Main()
+        {
+            bool content = true;
+            bool noContent = false;
+            Console.WriteLine("text is {0}",content);
+            Console.WriteLine("text is {0}",noContent);
+            Console.ReadKey();
+        }
+    }
+}
